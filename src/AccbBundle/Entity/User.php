8f16b86422f6b6c4bb5260e53db74a077d9c1d05
huijie <huijie.ye@supdeweb-paris.com>
2017-12-12 16:05:24 +0100

    integrer FORUSEBUNDLE

---
+++ src/AccbBundle/Entity/User.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AccbBundle\Entity;
+
+use FOS\UserBundle\Model as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $placeBirth;
+
+    /**
+     * @var string
+     */
+    private $job;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set placeBirth
+     *
+     * @param string $placeBirth
+     *
+     * @return User
+     */
+    public function setPlaceBirth($placeBirth)
+    {
+        $this->placeBirth = $placeBirth;
+
+        return $this;
+    }
+
+    /**
+     * Get placeBirth
+     *
+     * @return string
+     */
+    public function getPlaceBirth()
+    {
+        return $this->placeBirth;
+    }
+
+    /**
+     * Set job
+     *
+     * @param string $job
+     *
+     * @return User
+     */
+    public function setJob($job)
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+
+    /**
+     * Get job
+     *
+     * @return string
+     */
+    public function getJob()
+    {
+        return $this->job;
+    }
+}
+
